742f0ede7c214daac23181f65ba6b161f5142b22
Blake Blackwell <magiccatinteractive@gmail.com>
2018-05-18 23:07:44 -0400

    [ADDED]Scatterplot Solver.py
    A program to solve a scatter plot problem in my college math class.

---
+++ Scatterplot Solver.py
@@ -0,0 +1,98 @@
+'''import numpy as np
+import math
+N = 70
+x = np.random.rand(N)
+y = np.random.rand(N)
+colors = np.random.rand(N)
+area = np.pi * (15 * np.random.rand(N))**2 # 0 to 15 point radiuses
+
+#plt.scatter(x, y, s=area, c=colors, alpha=0.5)
+#plt.show()
+
+'''
+#Created by Blake Blackwell
+#For age groups scatter plot MAT90 December 2017 
+
+
+
+cats = (21.0/100)
+tabby = (29.5/100)
+mainecoon = (39.5/27)
+himalyan = (49.5/27)
+p1 = ("35%")
+p2 = ("32%")
+p3 = ("1.46296296296%")
+p4 = ("1.8333333333333333%")
+p4R = (1.8333333333333333*18)
+
+
+def ageGroup1(self):
+
+
+  def ageGroup2(self):
+
+
+    class ageOfAmericans(ageGroup1,ageGroup2):
+
+      def ageGroup1(self):
+        for age in range (18,25):
+         print (age)
+    
+    while age in range (18,25):
+      print cats
+      if cats == 0.21:
+        print ("The percent is " + p1)
+        break
+      continue
+     
+    
+ageGroup1(0)
+
+
+def ageGroup2(self):
+  for age in range (25,35):
+    print (age)
+    
+    while age in range (25,35):
+      print (tabby)
+      if tabby == 0.295:
+        print ("The percent is " +p2)
+        break
+        continue
+      
+       
+        
+      
+      
+      
+ageGroup2(1)
+
+
+
+
+def ageGroup3(self):
+  for age in range (35,45):
+    print (mainecoon)
+    if mainecoon == 1.46296296296:
+      print ("The percent is " +p3)
+      break
+      continue
+      
+     
+  
+  
+ageGroup3(2)
+
+
+
+
+def ageGroup4(self):
+  for age in range (45,55):
+    print (himalyan)
+    if himalyan == 1.8333333333333333:
+      print ("The percent is " +p4)
+      print str(("Real is: ")), p4R
+      break
+      continue
+    
+ageGroup4(3)
